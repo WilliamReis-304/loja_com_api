59ffcf09fbccf8682899cc255618aa1e352d9d25
unknown <williamsil304@gmail.com>
2024-08-20 16:35:48 -0300

    atualização para manter trajeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,6 @@ async function fetchData(){
 
 }
 
-fetchData()
-
 function criarVitrine(obj){
     divProdutos = document.getElementById('produtos')
     divProduto = document.createElement("div")
@@ -28,8 +26,36 @@ function criarVitrine(obj){
     botao = document.createElement('button')
     botao.innerText = 'Adicionar ao Carrinho +'
     botao.classList = "btn btn-outline-success"
+    botao.id = obj.id
     divImagem.append(img)
     divProduto.append(divImagem,h1,avaliacao,preco,descricao,botao)
     divProdutos.append(divProduto)
 }
 
+function adicionarItem(obj){
+    let carrinho = document
+    divCarrinho = carrinho.getElementById('carrinho')
+    listaCarrinho = carrinho.getElementById('ol')
+    liItens = carrinho.createElement("li")
+    liItens.id='itens'
+    divImagem = carrinho.createElement("div")
+    divImagem.id = 'imagem'
+    img = carrinho.createElement('img')
+    img.src = obj.image
+    preco = carrinho.createElement('h3')
+    preco.innerText = 'U$ ' + obj.price
+    h1 = carrinho.createElement('h1')
+    h1.innerText = obj.title
+    descricao = carrinho.createElement('p')
+    descricao.innerText = obj.description
+    botao = carrinho.createElement('button')
+    botao.innerText = 'Remover Item do Carrinho -'
+    botao.classList = "btn btn-outline-danger"
+    divImagem.append(img)
+    divProduto.append(divImagem,h1,avaliacao,preco,descricao,botao)
+    divCarrinho.append(divItens)
+}
+
+fetchData()
+
+botao.addEventListener("click",adicionarItem(this.id))
